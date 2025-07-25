891137058ba170f01364d8691101e49b669d90a8
Vladyslav Karakay <hitechnic00074220@gmail.com>
2025-07-25 20:40:50 +0000

    Add UI element Logo

---
+++ src/components/UI/Logo.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="wrapper">
+        <img :src="icon" alt="logotype" />
+        <h5 class="title">{{ title }}</h5>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Logotype from "../../assets/icons/logotype.png";
+
+export default defineComponent({
+    name: "Logo",
+    props: {
+        icon: {
+            type: String,
+            default: Logotype,
+            required: false,
+        },
+        title: {
+            type: String,
+            default: "Personal",
+            required: false,
+        },
+    },
+});
+</script>
+
+<style lang="scss">
+    @use "/src/assets/styles/fonts" as *;
+    @use "/src/assets/styles/colors" as *;
+
+    .wrapper {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 12px;
+        padding: 6px 0;
+
+        img {
+            width: max-content;
+            height: 40px;
+        }
+
+        h5 {
+            @include sora-font(700);
+            @include responsive-font(20, 20, 1440);
+            color: $primary-black;
+            margin: 0;
+        }
+
+        @media (max-width: 768px) {
+            gap: 8px;
+
+            img {
+                height: 28px;
+            }
+        }
+    }
+</style>
