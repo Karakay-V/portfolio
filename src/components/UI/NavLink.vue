f4ff5e621a56974040aa204d21faabc1c90d6148
Vladyslav Karakai <hitechnic00074220@gmail.com>
2025-07-27 12:05:46 +0300

    Add UI element NavLink

---
+++ src/components/UI/NavLink.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="wrapper">
+        <button :class="`${isSelected ? 'selected' : ''}`" 
+            v-scroll-to="{
+                element: to,
+                duration: 1200,
+                easing: 'easeInOutCubic',
+                offset: -60
+            }"
+        >
+            {{ label }}
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "NavLink",
+    props: {
+        to: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        isSelected: {
+            type: Boolean,
+            default: false,
+            required: false,
+        },
+    },
+});
+</script>
+
+<style lang="scss">
+@use "/src/assets/styles/fonts";
+@use "/src/assets/styles/colors" as *;
+
+.wrapper {
+    padding: 5px 0;
+    
+    button {
+        border: none;
+        outline: none;
+        margin: 0;
+        padding: 0;
+        background-color: transparent;
+        cursor: pointer;
+        transition: 0.3s ease color, 0.3s ease border-bottom;
+        @include fonts.sora-font(600);
+        @include fonts.responsive-font(20, 20, 1440);
+        color: $primary-black;
+
+        &:hover {
+            color: $primary-neutral;
+        }
+
+        &.selected {
+            border-bottom: 1px solid $primary-neutral;
+        }
+    }
+}
+</style>
