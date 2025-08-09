123afc3ac3b2957645a63b5e44e593dcedbc37d6
Vladyslav Karakay <hitechnic00074220@gmail.com>
2025-08-09 15:24:08 +0000

    add SkillsSection component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <Header />
   <HeroSection />
+  <SkillsSection />
 </template>
 
 <script setup lang="ts">
 import Header from './components/panels/Header.vue';
 import HeroSection from './components/panels/HeroSection.vue';
+import SkillsSection from './components/panels/SkillsSection.vue';
 </script>
 
 <style>
   body {
     margin: 0 auto;
     max-width: 1920px;
+  }
+
+  ::selection {
+    filter: invert(100%);
+  }
+
+  ::-moz-selection {
+    filter: invert(100%);
   }
 </style>
---
+++ src/components/panels/SkillsSection.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="inner-skills_section">
+        <div data-aos="fade-down" class="wrapper-title">
+            <h2 class="title">
+                <span class="display-regular">My</span>
+                <span class="display-extra_bold"> Skills</span>
+            </h2>
+        </div>
+
+        <div data-aos="fade-up" class="wrapper-skills_section">
+
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+            <Skill name="GitHub" :icon="GitHubIcon" :is-focused="false" />
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Skill from '../UI/Skill.vue';
+import GitHubIcon from '../../assets/icons/github.png';
+
+export default defineComponent({
+    name: "SkillsSection",
+    data() {
+        return({
+            GitHubIcon,
+        });
+    },
+    components: {
+        Skill,
+    },
+});
+</script>
+
+<style scoped lang="scss">
+@use "/src/assets/styles/fonts";
+@use "/src/assets/styles/colors" as *;
+@use "/src/assets/styles/image";
+
+.inner-skills_section {
+    padding: 60px 80px;
+
+    @media (max-width: 1260px) {
+        padding: 40px 16px;
+    }
+
+    @media (max-width: 390px) {
+        padding: 40px 0;   
+    }
+}
+
+.wrapper-title {
+    padding: 20px 0;
+
+    h2.title {
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        white-space: nowrap;
+        @include fonts.responsive-font(48, 28, 1440);
+        @include fonts.prevent-selecting;
+        
+        span {
+            &.display-regular {
+                @include fonts.sora-font(400);        
+            }
+
+            &.display-extra_bold {
+                @include fonts.sora-font(800);
+            }
+        }
+    }
+}
+
+.wrapper-skills_section {
+    margin-top: 20px;
+    padding: 20px 0;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(196px, 196px));
+    gap: 40px 71px;
+    justify-content: center;
+
+    max-width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(auto-fit, minmax(165px, 165px));
+        padding: 0;
+        gap: 20px;    
+    }
+}
+
+</style>
